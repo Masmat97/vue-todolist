901e8cce207d5943ddeef6d798d314455f545ee2
Matteo Mascio <matteo.mascio@libero.it>
2024-05-14 15:39:24 +0200

    M0

--- js/main.js
+++ js/main.js
@@ -2,13 +2,25 @@ const { createApp } = Vue
 
 createApp({
     data() {
-        return{
+        return {
+            lists:[
+                "Fare la spesa",
+                "Fare le pulizie",
+                "Preparare il pranzo",
+                "Lavare i piatti",
+                "Lavare i panni",
+                "Andare in palestra",
+                "Fare la doccia"
+            ],
 
-        },
+
+        }
     },
+
 methods: {
 
 },
+
 mounted() {
 
 }
